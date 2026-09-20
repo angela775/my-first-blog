09ab739e5ea61d457d439a7933020ceddb970631
angela775 <angela_j2p@hotmail.com>
2026-09-19 22:46:45 -0500

    Corregir ALLOWED_HOSTS para produccion

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['://pythonanywhere.com', '127.0.0.1']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
